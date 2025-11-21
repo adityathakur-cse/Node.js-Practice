ad8b3ebe66129b496514328fda678916966e3ac7
Aditya Kumar Thakur <adithakurji@gmail.com>
2025-11-21 15:08:13 +0530

    Add missing logout route and correct session flow

---
+++ form.js
@@ -0,0 +1,46 @@
+var session = require('cookie-session')
+var express = require('express')
+var app = new express()
+const port = 3000
+
+app.use(express.urlencoded({extended:true}))
+
+//cookie-session middleware
+app.use(session({
+    name: "mysession",
+    keys: ['secretKey123'],
+    maxAge: 24 * 60 * 60 * 1000 //24hrs
+}))
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/form.html")
+})
+
+//save the data
+app.post("/login", (req, res) => {
+    req.session.username = req.body.username
+    res.send(`Login stored in the cookie! <a href='/profile'>GO TO PROFILE</a>`)
+})
+
+//profile page
+app.get('/profile', (req, res) => {
+    if (!req.session.username) {
+        return res.send(`NO SESSION FOUND <a href='/'>LOGIN AGAIN</a>`)
+    }
+
+    res.send(`Welcome ${req.session.username}!
+        <br>
+        <a href='/logout'>LOGOUT!</a>
+        `)
+})
+
+//logout
+app.get('/logout', (req, res) => {
+    req.session = null
+    res.send(`Logged out successfully! <a href='/'>LOGIN AGAIN</a>`)
+})
+
+//start server 
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
